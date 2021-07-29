1c48665e76cab3d5fe3efdfc7e2630ed8ca6b84d
vbprofile <vbaltins39@gmail.com>
2021-07-29 23:06:23 +0300

    pievienoju 4404 ar plus one more;

--- src/main/java/GitOne.java
+++ src/main/java/GitOne.java
@@ -1,5 +1,7 @@
 public class GitOne {
     public static void main(String[] args) {
         System.out.println("GitOne Hallo!44 is commited in branch 04");
+        System.out.println("GitOne Hallo!44 is comited in branch 04");
+        System.out.println("plus one more ");
     }
 }
