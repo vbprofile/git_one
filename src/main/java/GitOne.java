8c2668cf75e26ec9c1959c8cf6e02bf4f0ed0815
vbprofile <vbaltins39@gmail.com>
2021-07-29 23:41:07 +0300

    this is ABC training;

--- src/main/java/GitOne.java
+++ src/main/java/GitOne.java
@@ -3,5 +3,6 @@ public class GitOne {
         System.out.println("GitOne Hallo!44 is commited in branch 04");
         System.out.println("GitOne Hallo!44 is comited in branch 04");
         System.out.println("plus one more ");
+        System.out.println("ABC training");
     }
 }
