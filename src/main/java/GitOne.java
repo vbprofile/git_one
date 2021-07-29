0a8e39c587f577433d9431810db7942002d1f9ba
vbprofile <vbaltins39@gmail.com>
2021-07-30 00:36:56 +0300

    this is NNNN training;

--- src/main/java/GitOne.java
+++ src/main/java/GitOne.java
@@ -1,9 +1,7 @@
 public class GitOne {
     public static void main(String[] args) {
 
-        System.out.println("ABC training");
-        System.out.println("DEF training");
-        System.out.println("GHI training");
+        System.out.println("NNNN");
 
     }
 }
