d254303e8848ae16352f39c7ca7352e92c705c7c
vbprofile <vbaltins39@gmail.com>
2021-07-30 20:08:06 +0300

    this is AAA,BBB, CCC training;

--- src/main/java/GitOne.java
+++ src/main/java/GitOne.java
@@ -2,9 +2,7 @@ public class GitOne {
     public static void main(String[] args) {
 
         System.out.println("NNNN");
-        System.out.println("AAA");
-        System.out.println("BBB");
-        System.out.println("CCC");
+
 
     }
 }
